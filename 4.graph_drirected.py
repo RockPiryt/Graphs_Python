ae4503fd60a439a9409965dfdd99b6eb311f759e
RockPiryt <rockpiryt@gmail.com>
2024-10-19 11:48:04 +0200

    rename file and add description of the task

--- 4.graph_drirected.py
+++ 4.graph_drirected.py
@@ -1,9 +1,39 @@
+'''Zadanie 6 (0.1 pkt)
+
+Napisz program, który utworzy graf skierowany dla ciągów liczb 
+S i A o następującej definicji:
+
+Zbiorem wierzchołków grafu będą liczby, które należą do ciągu A
+Istnieje łuk 
+(x,y), jeśli x≠y oraz  y−x∈S.
+Graf przedstaw w postaci macierzy sąsiedztwa.
+
+Pierwszy wiersz na wejściu jest to ciąg A, natomiast drugi wiersz reprezentuje ciąg S.
+
+Uwaga. Ciągi ze zbioru A mogą być nie posortowane. 
+Nie zmieniajmy kolejności występowania wierzchołków. 
+Czyli traktujemy jako pierwszy wierzchołek, ten który występuje pierwszy w ciągu liczb A
+
+Sample Input:
+
+0 1 2 3 4
+-2 1 2 4
+Sample Output:
+
+0 1 1 0 1
+0 0 1 1 0
+1 0 0 1 1
+0 1 0 0 1
+0 0 1 0 0'''
+
 def is_in_set(value, S):
     """Sprawdza, czy dana wartość znajduje się w zbiorze S."""
     return value in S
 
 def create_adjacency_matrix(A, S):
     """Tworzy macierz sąsiedztwa dla grafu skierowanego na podstawie zbiorów A i S."""
+    # A=[0 1 2 3 4]
+    # S=[-2 1 2 4]
     n = len(A)
     
     # Inicjalizuj pustą macierz sąsiedztwa zerami
@@ -11,8 +41,8 @@ def create_adjacency_matrix(A, S):
     for i in range(n):
         row = []
         for j in range(n):
-            row.append(0)  # Dodaj 0 do wiersza
-        adj_matrix.append(row)  # Dodaj wiersz do macierzy
+            row.append(0)  
+        adj_matrix.append(row)  
 
     # Wypełnij macierz sąsiedztwa
     for i in range(n):
@@ -44,12 +74,14 @@ def display_adjacency_matrix(adj_matrix):
         print()
 
 # Wejście: lista liczb A (pierwszy wiersz) oraz lista liczb S (drugi wiersz)
-A = [int(x) for x in input("Podaj elementy zbioru A (oddzielone spacją): ").split()]
-S = [int(x) for x in input("Podaj elementy zbioru S (oddzielone spacją): ").split()]
+print("Podaj elementy zbioru A (oddzielone spacją): ")
+A = [int(x) for x in input().split()]
+print("Podaj elementy zbioru S (oddzielone spacją): ")
+S = [int(x) for x in input().split()]
 
 # Utwórz macierz sąsiedztwa
 adj_matrix = create_adjacency_matrix(A, S)
 
 # Wyświetl macierz sąsiedztwa
-print("Macierz sąsiedztwa:")
+#print("Macierz sąsiedztwa:")
 display_adjacency_matrix(adj_matrix)
